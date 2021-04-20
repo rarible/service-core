5f8d5abeb39dd72794c751b723b6d695928cf67f
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-04-20 16:06:48 +0300

    ConversionService ext

---
+++ common/src/main/java/com/rarible/core/common/ConversionServiceExtensions.kt
@@ -0,0 +1,9 @@
+package com.rarible.core.common
+
+import org.springframework.core.convert.ConversionService
+
+inline fun <reified T> ConversionService.convert(source: Any): T {
+    return this.convert(source, T::class.java) ?: error("Can't convert ${source.javaClass} to ${T::class.java}")
+}
+
+
