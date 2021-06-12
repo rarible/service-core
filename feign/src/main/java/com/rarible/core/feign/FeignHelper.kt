11dce93fd864c8817abf9de252ccc1da056509fd
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-06-12 15:13:13 +0300

    fix feign helper

--- feign/src/main/java/com/rarible/core/feign/FeignHelper.kt
+++ feign/src/main/java/com/rarible/core/feign/FeignHelper.kt
@@ -1,8 +1,11 @@
 package com.rarible.core.feign
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.netty.channel.ChannelOption
+import io.netty.channel.epoll.EpollChannelOption
 import org.springframework.cloud.openfeign.support.SpringMvcContract
 import org.springframework.http.MediaType
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.http.codec.ClientCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
@@ -10,6 +13,7 @@ import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 import reactivefeign.ReactiveContract
 import reactivefeign.webclient.WebReactiveFeign
+import reactor.netty.http.client.HttpClient
 
 object FeignHelper {
     fun <T> createClient(clazz: Class<T>, mapper: ObjectMapper, baseUrl: String): T {
@@ -24,8 +28,15 @@ object FeignHelper {
                 )
             }.build()
 
+        val client = HttpClient.create().tcpConfiguration {
+            it.option(ChannelOption.SO_KEEPALIVE, true)
+                .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+                .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+                .option(EpollChannelOption.TCP_KEEPCNT, 8)
+        }
+        val connector = ReactorClientHttpConnector(client)
         return WebReactiveFeign
-            .builder<T>(WebClient.builder().exchangeStrategies(strategies))
+            .builder<T>(WebClient.builder().clientConnector(connector).exchangeStrategies(strategies))
             .contract(ReactiveContract(SpringMvcContract()))
             .target(clazz, baseUrl)
     }
