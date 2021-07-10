097b9f412a2ab235115f90f0173cb5f7b187d920
sergey.anufriev <s.anufriev@rarible.com>
2021-07-10 12:57:16 +0300

    Remove unneeded import

--- reduce/src/main/kotlin/com/rarible/core/reduce/service/ReduceService.kt
+++ reduce/src/main/kotlin/com/rarible/core/reduce/service/ReduceService.kt
@@ -10,14 +10,12 @@ import com.rarible.core.reduce.repository.ReduceEventRepository
 import com.rarible.core.reduce.repository.SnapshotRepository
 import kotlinx.coroutines.flow.fold
 import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactor.mono
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toFlux
-import java.lang.RuntimeException
 
 class ReduceService<
         Event : ReduceEvent<Mark>,
