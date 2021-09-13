6e90c099a1e09f5152c52b8f9de86d325aa537c4
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-09-13 17:09:28 +0300

    docs(task-service): add JavaDoc for TaskHandler. (#11)

--- task/src/main/java/com/rarible/core/task/TaskHandler.kt
+++ task/src/main/java/com/rarible/core/task/TaskHandler.kt
@@ -2,15 +2,50 @@ package com.rarible.core.task
 
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Background handler of tasks of a specific [type]. Tasks are run by [TaskService].
+ * There may be several tasks of this type with different `param` running in the background simultaneously.
+ * Task is unique by a [type] and `param` with which it was started.
+ * Task completion state returned from [runLongTask] and is recorded in the database to resume execution from the last state.
+ *
+ * Override this interface in a class marked with `@Component` or `@Service`.
+ * Spring will pick up and register this handler automatically.
+ *
+ * There are 2 options how to schedule this task for execution:
+ * - run [TaskService.runTask] with `type` parameter equal to this handler's [type] and a parameter to run with.
+ * - override [getAutorunParams] with non-empty list of params. [TaskService] will schedule the task for execution
+ * after a fixed delay on the application startup (usually after 30 seconds).
+ */
 interface TaskHandler<T: Any> {
+    /**
+     * Application-wide unique identifier of this type of tasks.
+     */
     val type: String
+
+    /**
+     * Whether this task should be run right now.
+     */
     suspend fun isAbleToRun(param: String): Boolean {
         return true
     }
+
+    /**
+     * Parameters for auto-running the task after a fixed delay on application startup.
+     * Override this function to automatically start the background task.
+     */
     fun getAutorunParams() = emptyList<RunTask>()
+
+    /**
+     * Task execution logic.
+     * The task may be resumed [from] the latest state, which is recorded in the database.
+     * The task state is updated every time the resulting `Flow<T>` emits the next element.
+     */
     fun runLongTask(from: T?, param: String): Flow<T>
 }
 
+/**
+ * Auto-run parameters of the task, used as return type [TaskHandler.getAutorunParams].
+ */
 data class RunTask(
     val param: String,
     val sample: Long? = Task.DEFAULT_SAMPLE
