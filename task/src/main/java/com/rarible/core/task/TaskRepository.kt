836900dfb4d4ea2b70d265d34a33a56020a7a0ae
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-09-13 17:06:05 +0300

    docs(task-service): add JavaDoc for TaskHandler.

--- task/src/main/java/com/rarible/core/task/TaskRepository.kt
+++ task/src/main/java/com/rarible/core/task/TaskRepository.kt
@@ -4,6 +4,11 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+/**
+ * NOT A PUBLIC API.
+ *
+ * Internal database repository for [Task]s.
+ */
 interface TaskRepository : ReactiveCrudRepository<Task, String> {
     fun findByTypeAndParam(type: String, param: String): Mono<Task>
     fun findByRunning(running: Boolean): Flux<Task>
