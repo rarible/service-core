d076e12bd51253bdd3fea76bcd90a9f2303c0f3c
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-10-13 13:08:54 +0300

    fix: removed unneeded logging

--- lock-redis/src/main/java/com/rarible/core/lockredis/service.kt
+++ lock-redis/src/main/java/com/rarible/core/lockredis/service.kt
@@ -2,11 +2,8 @@ package com.rarible.core.lockredis
 
 import com.rarible.core.common.toOptional
 import com.rarible.core.lock.LockService
-import com.rarible.core.logging.LoggingUtils
 import io.lettuce.core.SetArgs
 import io.lettuce.core.api.reactive.RedisReactiveCommands
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.switchIfEmpty
@@ -33,46 +30,32 @@ class RedisLockService(
     }
 
     private fun acquireLock(key: String, expiresMs: Int): Mono<Long> {
-        return LoggingUtils.withMarker { marker ->
-            logger.info(marker, "acquireLock $key expiresMs=$expiresMs")
-            val id = Random.nextLong(0, Long.MAX_VALUE)
-            tryLock(key, id, expiresMs)
-                .thenReturn(id)
-                .retryWhen(backoff)
-        }
+        val id = Random.nextLong(0, Long.MAX_VALUE)
+        return tryLock(key, id, expiresMs)
+            .thenReturn(id)
+            .retryWhen(backoff)
     }
 
     @Suppress("RemoveExplicitTypeArguments")
     private fun tryLock(key: String, id: Long, expiresMs: Int): Mono<Void> {
-        return LoggingUtils.withMarker { marker ->
-            logger.info(marker, "tryLock key=$key id=$id expiresMs=$expiresMs")
-            client.set(key, id.toString(), SetArgs().nx().px(expiresMs.toLong()))
-                .toOptional()
-                .flatMap<Void> {
-                    if (it.isPresent && it.get() == "OK")
-                        Mono.empty()
-                    else
-                        Mono.error(IllegalStateException("lock $key is not available"))
-                }
-        }
+        return client.set(key, id.toString(), SetArgs().nx().px(expiresMs.toLong()))
+            .toOptional()
+            .flatMap<Void> {
+                if (it.isPresent && it.get() == "OK")
+                    Mono.empty()
+                else
+                    Mono.error(IllegalStateException("lock $key is not available"))
+            }
     }
 
     private fun unlock(key: String, id: Long): Mono<Void> {
-        return LoggingUtils.withMarker { marker ->
-            logger.info(marker, "unlock $key id=$id")
-            client.get(key)
-                .flatMap {
-                    if (it == id.toString()) {
-                        client.del(key).then()
-                    } else {
-                        Mono.empty()
-                    }
+        return client.get(key)
+            .flatMap {
+                if (it == id.toString()) {
+                    client.del(key).then()
+                } else {
+                    Mono.empty()
                 }
-        }
-    }
-
-    companion object {
-        @Suppress("unused")
-        val logger: Logger = LoggerFactory.getLogger(RedisLockService::class.java)
+            }
     }
 }
