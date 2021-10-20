876476ff970c96a6e1ee0eaf196602a1439729a0
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-20 07:50:29 +0300

    Add BigIntegerToStringSerializer [RPN-1158].

--- mongo/src/main/java/com/rarible/core/mongo/configuration/MongoConfiguration.java
+++ mongo/src/main/java/com/rarible/core/mongo/configuration/MongoConfiguration.java
@@ -1,6 +1,7 @@
 package com.rarible.core.mongo.configuration;
 
 import com.rarible.core.mongo.converter.CustomConversionsFactory;
+import com.rarible.core.mongo.jackson.BigIntegerToStringSerializer;
 import com.rarible.core.mongo.jackson.ObjectIdCombinedSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import com.rarible.core.mongo.migrate.MongoIndicesService;
 @Import({
     ConvertersConfiguration.class,
     ObjectIdCombinedSerializer.class,
+    BigIntegerToStringSerializer.class,
     MongoIndicesService.class
 })
 public class MongoConfiguration {
