731b48fda79e129186f965ed1fa046e83b187d83
sergey.anufriev <s.anufriev@rarible.com>
2021-10-20 19:56:13 +0300

    Add apm starter

---
+++ apm-starter/src/main/kotlin/com/rarible/core/apm/agent/autoconfigure/ApmAgentProperties.kt
@@ -0,0 +1,15 @@
+package com.rarible.core.apm.agent.autoconfigure
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.net.URI
+
+internal const val RARIBLE_APM_AGENT = "rarible.core.apm.agent"
+
+@ConstructorBinding
+@ConfigurationProperties(RARIBLE_APM_AGENT)
+data class ApmAgentProperties(
+    val enabled: Boolean = false,
+    val server: URI = URI.create("http://elastic-apm:8200"),
+    val packages: String? = null
+)
