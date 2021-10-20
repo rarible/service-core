3b0533a0f2fd2ffe8cbe2603cfcb7780f13f7139
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-10-20 15:51:05 +0300

    fix: apm span labels support

--- apm/src/main/kotlin/com/rarible/core/apm/ApmUtils.kt
+++ apm/src/main/kotlin/com/rarible/core/apm/ApmUtils.kt
@@ -18,32 +18,51 @@ suspend fun <T> withSpan(
         subType = info.subType,
         action = info.subType,
         name = info.name,
+        labels = info.labels,
         body = body
     )
 }
 
 suspend fun <T> withSpan(
     name: String,
-    type: String,
+    type: String? = null,
     subType: String? = null,
     action: String? = null,
+    labels: List<Pair<String, Any>> = emptyList(),
     body: suspend () -> T
 ): T {
     val ctx = getApmContext()
     return if (ctx != null) {
         val span = ctx.span.startSpan(type, subType, action)
         span.setName(name)
+        for ((key, value) in labels) {
+            when(value) {
+                is Number -> span.setLabel(key, value)
+                is String -> span.setLabel(key, value)
+                is Boolean -> span.setLabel(key, value)
+            }
+        }
         span.using(body)
     } else {
         body()
     }
 }
 
-suspend fun <T> withTransaction(name: String, body: suspend () -> T): T {
-    val transaction = ElasticApm.startTransaction()
-    transaction.setName(name)
-
-    return transaction.using(body)
+suspend fun <T> withTransaction(
+    name: String,
+    labels: List<Pair<String, Any>> = emptyList(),
+    body: suspend () -> T
+): T {
+    val span = ElasticApm.startTransaction()
+    span.setName(name)
+    for ((key, value) in labels) {
+        when(value) {
+            is Number -> span.setLabel(key, value)
+            is String -> span.setLabel(key, value)
+            is Boolean -> span.setLabel(key, value)
+        }
+    }
+    return span.using(body)
 }
 
 suspend fun <T> Span.using(body: suspend () -> T): T {
--- apm/src/main/kotlin/com/rarible/core/apm/SpanInfo.kt
+++ apm/src/main/kotlin/com/rarible/core/apm/SpanInfo.kt
@@ -2,7 +2,8 @@ package com.rarible.core.apm
 
 data class SpanInfo(
     val name: String,
-    val type: String,
+    val type: String? = null,
     val subType: String? = null,
-    val action: String? = null
+    val action: String? = null,
+    val labels: List<Pair<String, Any>> = emptyList()
 )
