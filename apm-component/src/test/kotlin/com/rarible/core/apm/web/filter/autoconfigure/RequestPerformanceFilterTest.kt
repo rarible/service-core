f3b91df75aa90fe07e48be6dcb5c2f36658521a5
sergey.anufriev <s.anufriev@rarible.com>
2021-10-25 22:09:05 +0700

    Rename package for apm component

--- apm-component/src/test/kotlin/com/rarible/core/apm/web/filter/autoconfigure/RequestPerformanceFilterTest.kt
+++ apm-component/src/test/kotlin/com/rarible/core/apm/web/filter/autoconfigure/RequestPerformanceFilterTest.kt
@@ -1,15 +1,9 @@
 package com.rarible.core.apm.web.filter.autoconfigure
 
-import co.elastic.apm.api.ElasticApm
-import co.elastic.apm.api.Transaction
 import com.rarible.core.apm.*
 import com.rarible.core.apm.web.filter.RequestPerformanceFilter
 import com.rarible.core.application.ApplicationEnvironmentInfo
 import com.rarible.core.application.ApplicationInfo
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,13 +18,11 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
-import reactor.core.publisher.Mono
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = [
-        "rarible.core.filter.apm.enabled=true",
-        "rarible.core.apm.annotation.enabled=true"
+        "rarible.core.filter.apm.enabled=true"
     ]
 )
 @SpringBootConfiguration
@@ -44,9 +36,6 @@ class RequestPerformanceFilterTest {
     @Autowired
     private lateinit var requestPerformanceFilter: RequestPerformanceFilter
 
-    @Autowired
-    private lateinit var testAnnotatedClass: TestAnnotatedClass
-
     @Test
     fun `test apm filter initialized`() {
         assertThat(requestPerformanceFilter).isNotNull
@@ -57,22 +46,6 @@ class RequestPerformanceFilterTest {
         val template = RestTemplate()
         val result: Boolean = template.getForObject("http://localhost:${port}/test")
         assertThat(result).isTrue()
-    }
-
-    @Test
-    fun `should handle transaction annotation`() {
-        mockkStatic(ElasticApm::class)
-        val transaction = mockk<Transaction>()
-        every { transaction.setName("testTransaction") } returns transaction
-        every { transaction.end() } returns Unit
-
-        every { ElasticApm.startTransaction() } returns transaction
-
-        testAnnotatedClass.openTransaction().block()
-
-        verify(exactly = 1) { ElasticApm.startTransaction() }
-        verify(exactly = 1) { transaction.setName("testTransaction") }
-        verify(exactly = 1) { transaction.end() }
     }
 
     @TestConfiguration
@@ -86,11 +59,6 @@ class RequestPerformanceFilterTest {
         fun applicationInfo(): ApplicationInfo {
             return ApplicationInfo("test", "test.com")
         }
-
-        @Bean
-        fun testAnnotatedClass(): TestAnnotatedClass {
-            return TestAnnotatedClass()
-        }
     }
 
     @RestController
@@ -99,19 +67,6 @@ class RequestPerformanceFilterTest {
         suspend fun test(): Boolean {
             val ctx = getApmContext()
             return ctx != null
-        }
-    }
-
-    @Spanable
-    open class TestAnnotatedClass {
-        @CaptureTransaction("testTransaction")
-        open fun openTransaction(): Mono<String> {
-            return Mono.just("Open Transaction")
-        }
-
-        @CaptureSpan(value = "testName", type = "testType", subtype = "testSubType", action = "testAction")
-        open fun openSpan(): Mono<String> {
-            return Mono.just("Open Span")
         }
     }
 }
