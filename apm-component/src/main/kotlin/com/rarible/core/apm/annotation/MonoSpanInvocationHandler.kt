856d466a1c8ac2dc79fd3b6364326b5a93191b07
sergey.anufriev <s.anufriev@rarible.com>
2021-11-02 11:45:51 +0700

    Try to implement span proxy with suspend functions

--- apm-component/src/main/kotlin/com/rarible/core/apm/annotation/MonoSpanInvocationHandler.kt
+++ apm-component/src/main/kotlin/com/rarible/core/apm/annotation/MonoSpanInvocationHandler.kt
@@ -1,11 +1,18 @@
 package com.rarible.core.apm.annotation
 
+import co.elastic.apm.api.ElasticApm
+import co.elastic.apm.api.Span
 import com.rarible.core.apm.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.reactor.ReactorContext
 import org.aopalliance.intercept.MethodInterceptor
 import org.aopalliance.intercept.MethodInvocation
 import reactor.core.publisher.Mono
+import reactor.util.context.Context
 import java.lang.reflect.Method
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
 
 typealias MethodSignature = String
 
@@ -14,7 +21,7 @@ class MonoSpanInvocationHandler(
 ) : MethodInterceptor {
 
     private val checkCache = ConcurrentHashMap<MethodSignature, Boolean>()
-    private val infoCache = ConcurrentHashMap<MethodSignature, SpanCache>()
+    private val spanMethodCache = ConcurrentHashMap<MethodSignature, SpanMethod>()
 
     override fun invoke(invocation: MethodInvocation): Any {
         val method = invocation.method
@@ -25,20 +32,18 @@ class MonoSpanInvocationHandler(
                 invocation.proceed()
             }
             true -> {
-                val cache = infoCache[signature] ?: error("Cache must exist for signature $signature")
-                invoke(cache, invocation)
+                val spanMethod = spanMethodCache[signature] ?: error("Cache must exist for signature $signature")
+                spanMethod.invoke(invocation)
             }
             null -> {
                 val original = type.getMethod(method.name, *method.parameterTypes)
+                val spanMethod = extractSpanMethod(original, signature)
 
-                val validReturnType = method.returnType == Mono::class.java
-                val cache = extractCacheInfo(signature, original)
-
-                if (validReturnType && cache != null) {
+                if (spanMethod != null) {
                     checkCache[signature] = true
-                    infoCache[signature] = cache
+                    spanMethodCache[signature] = spanMethod
 
-                    invoke(cache, invocation)
+                    spanMethod.invoke(invocation)
                 } else {
                     checkCache[signature] = false
                     invocation.proceed()
@@ -47,29 +52,27 @@ class MonoSpanInvocationHandler(
         }
     }
 
-    private fun extractCacheInfo(methodSignature: MethodSignature, method: Method): SpanCache? {
+    private fun extractSpanMethod(method: Method, defaultName: String): SpanMethod? {
         return when {
             method.isAnnotationPresent(CaptureSpan::class.java) -> {
-                val spanAnnotation = method.getAnnotation(CaptureSpan::class.java)
-                SpanCache(
-                    type = SpanCache.Type.SPAN,
-                    info = SpanInfo(
-                        name = spanAnnotation.value.ifNotBlack() ?: methodSignature,
-                        type = spanAnnotation.type.ifNotBlack(),
-                        subType = spanAnnotation.subtype.ifNotBlack(),
-                        action = spanAnnotation.action.ifNotBlack(),
+                val info = method.getAnnotation(CaptureSpan::class.java).let { annotation ->
+                    SpanInfo(
+                        name = annotation.value.ifNotBlack() ?: defaultName,
+                        type = annotation.type.ifNotBlack(),
+                        subType = annotation.subtype.ifNotBlack(),
+                        action = annotation.action.ifNotBlack(),
                         labels = emptyList()
                     )
-                )
+                }
+                SpanMethod.createSpan(method, info)
             }
             method.isAnnotationPresent(CaptureTransaction::class.java) -> {
-                val spanAnnotation = method.getAnnotation(CaptureTransaction::class.java)
-                SpanCache(
-                    type = SpanCache.Type.TRANSACTION,
-                    info = SpanInfo(
-                        name = spanAnnotation.value.ifNotBlack() ?: methodSignature
+                val info = method.getAnnotation(CaptureTransaction::class.java).let { annotation ->
+                    SpanInfo(
+                        name = annotation.value.ifNotBlack() ?: defaultName
                     )
-                )
+                }
+                SpanMethod.createTransaction(method, info)
             }
             else -> null
         }
@@ -77,37 +80,140 @@ class MonoSpanInvocationHandler(
 
     private fun getMethodSignature(method: Method): MethodSignature = "${type.name}#${method.name}"
 
-    private fun invoke(spanCache: SpanCache, invocation: MethodInvocation): Any {
-        val result = invocation.proceed() as Mono<*>
-        val spanInfo = spanCache.info
-
-        return when (spanCache.type) {
-            SpanCache.Type.SPAN ->
-                result.withSpan(
-                    name = spanInfo.name,
-                    type = spanInfo.type,
-                    subType = spanInfo.subType,
-                    action = spanInfo.action,
-                    labels = spanInfo.labels
-                )
-
-            SpanCache.Type.TRANSACTION ->
-                result.withTransaction(
-                    name = spanInfo.name,
-                    labels = spanInfo.labels
-                )
-        }
-    }
-
     private fun String.ifNotBlack(): String? = ifBlank { null }
 
-    private data class SpanCache(
-        val info: SpanInfo,
-        val type: Type
-    ) {
-        enum class Type {
-            TRANSACTION,
-            SPAN
+    private sealed class SpanMethod {
+        protected abstract val info: SpanInfo
+
+        abstract fun invoke(invocation: MethodInvocation): Any
+
+        companion object {
+            fun createSpan(method: Method, spanInfo: SpanInfo): SpanMethod {
+                return when (getMethodType(method)) {
+                    MethodType.SUSPEND -> SuspendSpanMethod(spanInfo)
+                    MethodType.MONO -> MonoSpanMethod(spanInfo)
+                    MethodType.NORMAL -> TODO()
+                }
+            }
+
+            fun createTransaction(method: Method, spanInfo: SpanInfo): SpanMethod {
+                return when (getMethodType(method)) {
+                    MethodType.SUSPEND -> SuspendTransactionMethod(spanInfo)
+                    MethodType.MONO -> MonoTransactionMethod(spanInfo)
+                    MethodType.NORMAL -> TODO()
+                }
+            }
+
+            private fun getMethodType(method: Method): MethodType {
+                return when {
+                    method.returnType == Mono::class.java -> MethodType.MONO
+                    method.parameterCount > 0 && method.parameterTypes.last() == Continuation::class.java -> MethodType.SUSPEND
+                    else -> MethodType.NORMAL
+                }
+            }
+
+            enum class MethodType {
+                MONO,
+                SUSPEND,
+                NORMAL
+            }
+        }
+
+        data class MonoSpanMethod(
+            override val info: SpanInfo
+        ) : SpanMethod() {
+
+            override fun invoke(invocation: MethodInvocation): Any {
+                val result = invocation.proceed() as Mono<*>
+                return result.withSpan(
+                    name = info.name,
+                    type = info.type,
+                    subType = info.subType,
+                    action = info.action,
+                    labels = info.labels
+                )
+            }
+        }
+
+        data class MonoTransactionMethod(
+            override val info: SpanInfo
+        ) : SpanMethod() {
+
+            override fun invoke(invocation: MethodInvocation): Any {
+                val result = invocation.proceed() as Mono<*>
+                return result.withTransaction(
+                    name = info.name,
+                    labels = info.labels
+                )
+            }
+        }
+
+        data class SuspendSpanMethod(
+           override val info: SpanInfo
+        ) : SpanMethod() {
+
+            @ExperimentalCoroutinesApi
+            override fun invoke(invocation: MethodInvocation): Any {
+                val arguments = invocation.arguments
+                val continuation = arguments.last() as Continuation<*>
+
+                val reactorContext = continuation.context[ReactorContext.Key]?.context
+                val apmContext = reactorContext?.get<ApmContext>(ApmContext.Key)
+
+                if (apmContext != null) {
+                    val span = apmContext.span.startSpan(info.type, info.subType, info.action)
+                    span.setName(info.name)
+
+                    val spanWrappedContinuation = SpanWrappedContinuation(continuation, span, reactorContext)
+                    arguments[arguments.size - 1] = spanWrappedContinuation
+                }
+                return invocation.proceed()
+            }
+        }
+
+        data class SuspendTransactionMethod(
+            override val info: SpanInfo
+        ) : SpanMethod() {
+
+            @ExperimentalCoroutinesApi
+            override fun invoke(invocation: MethodInvocation): Any {
+                val arguments = invocation.arguments
+                val continuation = arguments.last() as Continuation<*>
+
+                val reactorContext = continuation.context[ReactorContext.Key]?.context ?: Context.empty()
+
+                val transaction = ElasticApm.startTransaction()
+                transaction.setName(info.name)
+
+                val spanWrappedContinuation = SpanWrappedContinuation(continuation, transaction, reactorContext)
+                arguments[arguments.size - 1] = spanWrappedContinuation
+
+                return invocation.proceed()
+            }
+        }
+
+        @ExperimentalCoroutinesApi
+        class SpanWrappedContinuation<T>(
+            private val continuation: Continuation<T>,
+            private val span: Span,
+            reactorContext: Context
+        ) : Continuation<T> {
+            private val contextWithSpan = putApmContext(span, reactorContext, continuation)
+
+            override val context: CoroutineContext
+                get() = contextWithSpan
+
+            override fun resumeWith(result: Result<T>) {
+                when {
+                    result.isSuccess -> span.end()
+                    result.isFailure -> span.captureException(result.exceptionOrNull())
+                }
+                continuation.resumeWith(result)
+            }
+
+            private fun putApmContext(span: Span, reactorContext: Context, continuation: Continuation<T>): CoroutineContext {
+                return continuation.context + ReactorContext(reactorContext.put(ApmContext.Key, ApmContext(span)))
+            }
         }
     }
 }
--- apm-component/src/test/kotlin/com/rarible/core/apm/annotation/SpanAnnotationPostProcessorTest.kt
+++ apm-component/src/test/kotlin/com/rarible/core/apm/annotation/SpanAnnotationPostProcessorTest.kt
@@ -1,6 +1,7 @@
 package com.rarible.core.apm.annotation
 
 import co.elastic.apm.api.ElasticApm
+import co.elastic.apm.api.Span
 import co.elastic.apm.api.Transaction
 import com.rarible.core.apm.CaptureSpan
 import com.rarible.core.apm.CaptureTransaction
@@ -11,6 +12,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.verify
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringBootConfiguration
@@ -31,10 +33,10 @@ import reactor.core.publisher.Mono
 @Import(SpanAnnotationPostProcessorTest.Configuration::class)
 class SpanAnnotationPostProcessorTest {
     @Autowired
-    private lateinit var testAnnotatedClass: TestAnnotatedClass
+    private lateinit var transactionClass: TransactionClass
 
     @Test
-    fun `should handle transaction annotation`() {
+    fun `should handle mono transaction annotation`() {
         mockkStatic(ElasticApm::class)
         val transaction = mockk<Transaction>()
         every { transaction.setName("testTransaction") } returns transaction
@@ -42,18 +44,64 @@ class SpanAnnotationPostProcessorTest {
 
         every { ElasticApm.startTransaction() } returns transaction
 
-        testAnnotatedClass.openTransaction().block()
+        transactionClass.openMonoTransaction().block()
 
         verify(exactly = 1) { ElasticApm.startTransaction() }
         verify(exactly = 1) { transaction.setName("testTransaction") }
         verify(exactly = 1) { transaction.end() }
     }
 
+    @Test
+    fun `should handle suspend transaction annotation`() = runBlocking<Unit> {
+        mockkStatic(ElasticApm::class)
+        val transaction = mockk<Transaction>()
+        every { transaction.setName("testTransaction") } returns transaction
+        every { transaction.end() } returns Unit
+
+        every { ElasticApm.startTransaction() } returns transaction
+
+        transactionClass.openSuspendTransaction()
+
+        verify(exactly = 1) { ElasticApm.startTransaction() }
+        verify(exactly = 1) { transaction.setName("testTransaction") }
+        verify(exactly = 1) { transaction.end() }
+    }
+
+    @Test
+    fun `should handle transaction and span annotation`() {
+        mockkStatic(ElasticApm::class)
+        val transaction = mockk<Transaction>()
+        val span = mockk<Span>()
+
+        every { transaction.setName("testTransaction") } returns transaction
+        every { transaction.end() } returns Unit
+        every { transaction.startSpan("testType", "testSubType", "testAction") } returns span
+
+        every { span.setName("testName") } returns span
+        every { span.end() } returns Unit
+
+        every { ElasticApm.startTransaction() } returns transaction
+
+        transactionClass.openMonoTransactionAndSpan().block()
+
+        verify(exactly = 1) { ElasticApm.startTransaction() }
+        verify(exactly = 1) { transaction.setName("testTransaction") }
+        verify(exactly = 1) { transaction.startSpan("testType", "testSubType", "testAction") }
+        verify(exactly = 1) { transaction.end() }
+        verify(exactly = 1) { span.setName("testName") }
+        verify(exactly = 1) { span.end() }
+    }
+
     @TestConfiguration
     internal class Configuration {
         @Bean
-        fun testAnnotatedClass(): TestAnnotatedClass {
-            return TestAnnotatedClass()
+        fun spanClass(): SpanClass {
+            return SpanClass()
+        }
+
+        @Bean
+        fun transactionClass(monoSpanClass: SpanClass): TransactionClass {
+            return TransactionClass(monoSpanClass)
         }
 
         @Bean
@@ -68,15 +116,42 @@ class SpanAnnotationPostProcessorTest {
     }
 
     @Spanable
-    open class TestAnnotatedClass {
+    open class TransactionClass(
+        private val spanClass: SpanClass
+    ) {
         @CaptureTransaction("testTransaction")
-        open fun openTransaction(): Mono<String> {
+        open fun openMonoTransaction(): Mono<String> {
             return Mono.just("Open Transaction")
         }
 
+        @CaptureTransaction("testTransaction")
+        open fun openMonoTransactionAndSpan(): Mono<String> {
+            return Mono
+                .just("Open Transaction")
+                .then(spanClass.openMonoSpan())
+        }
+
+        @CaptureTransaction("testTransaction")
+        open suspend fun openSuspendTransaction(): String {
+            return "Open Transaction"
+        }
+
+        @CaptureTransaction("testTransaction")
+        open suspend fun openSuspendTransactionAndSpan(): String {
+            return spanClass.openSuspendSpan()
+        }
+    }
+
+    @Spanable
+    open class SpanClass {
         @CaptureSpan(value = "testName", type = "testType", subtype = "testSubType", action = "testAction")
-        open fun openSpan(): Mono<String> {
+        open fun openMonoSpan(): Mono<String> {
             return Mono.just("Open Span")
+        }
+
+        @CaptureSpan(value = "testName", type = "testType", subtype = "testSubType", action = "testAction")
+        open suspend fun openSuspendSpan(): String {
+            return "Open Span"
         }
     }
 }
