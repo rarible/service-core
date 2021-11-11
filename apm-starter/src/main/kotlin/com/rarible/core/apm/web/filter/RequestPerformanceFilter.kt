8011d5576a410a8c7f6bd584b531594f719d89f5
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-11-11 08:51:12 +0300

    fix: improve apm filter

--- apm-starter/src/main/kotlin/com/rarible/core/apm/web/filter/RequestPerformanceFilter.kt
+++ apm-starter/src/main/kotlin/com/rarible/core/apm/web/filter/RequestPerformanceFilter.kt
@@ -3,18 +3,20 @@ package com.rarible.core.apm.web.filter
 import co.elastic.apm.api.HeaderExtractor
 import co.elastic.apm.api.HeadersExtractor
 import com.rarible.core.apm.withTransaction
-import com.rarible.core.application.ApplicationInfo
+import com.rarible.core.common.orNull
+import com.rarible.core.common.toOptional
 import org.springframework.core.Ordered
+import org.springframework.web.method.HandlerMethod
+import org.springframework.web.reactive.DispatcherHandler
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 class RequestPerformanceFilter(
-    applicationInfo: ApplicationInfo
+    private val dispatcherHandler: DispatcherHandler
 ) : WebFilter, Ordered {
-
-    private val service = applicationInfo.serviceName
 
     override fun getOrder() = Ordered.LOWEST_PRECEDENCE
 
@@ -26,11 +28,24 @@ class RequestPerformanceFilter(
         val path = request.path.pathWithinApplication().value()
         val method = request.methodValue
 
-        return chain.filter(exchange)
-            .withTransaction(
-                "$method#$path",
-                headerExtractor = HeaderExtractor { header -> headers.getFirst(header) },
-                headersExtractor = HeadersExtractor { header -> headers[header] }
-            )
+        return Flux.fromIterable(dispatcherHandler.handlerMappings)
+            .flatMap { it.getHandler(exchange) }.next().toOptional()
+            .flatMap {
+                val handler = it.orNull()
+                val defaultName = "$method $path"
+                val (name, labels) = if (handler is HandlerMethod) {
+                    "${handler.beanType.simpleName}#${handler.method.name}" to listOf("request" to defaultName)
+                } else {
+                    defaultName to emptyList()
+                }
+                println("name=$name label=${labels}")
+                chain.filter(exchange)
+                    .withTransaction(
+                        "$method#$path",
+                        labels = labels,
+                        headerExtractor = HeaderExtractor { header -> headers.getFirst(header) },
+                        headersExtractor = HeadersExtractor { header -> headers[header] }
+                    )
+            }
     }
 }
--- apm-starter/src/main/kotlin/com/rarible/core/apm/web/filter/autoconfigure/RequestPerformanceFilterConfiguration.kt
+++ apm-starter/src/main/kotlin/com/rarible/core/apm/web/filter/autoconfigure/RequestPerformanceFilterConfiguration.kt
@@ -1,10 +1,12 @@
 package com.rarible.core.apm.web.filter.autoconfigure
 
 import com.rarible.core.apm.web.filter.RequestPerformanceFilter
-import com.rarible.core.application.ApplicationInfo
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.DispatcherHandler
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping
 
 @ConditionalOnProperty(
     prefix = RARIBLE_FILTER_APM,
@@ -14,10 +16,15 @@ import org.springframework.context.annotation.Bean
 )
 @EnableConfigurationProperties(ApmFilterProperties::class)
 class RequestPerformanceFilterConfiguration(
-    private val applicationInfo: ApplicationInfo
+    private val handler: DispatcherHandler
 ) {
     @Bean
+    fun handlerMapping(): HandlerMapping {
+        return RequestMappingHandlerMapping()
+    }
+
+    @Bean
     fun requestPerformanceFilter(): RequestPerformanceFilter {
-        return RequestPerformanceFilter(applicationInfo)
+        return RequestPerformanceFilter(handler)
     }
 }
