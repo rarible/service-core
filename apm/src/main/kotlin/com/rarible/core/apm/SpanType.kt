ccac087b2e7f0982f5e0a764dbd90521e41bb6b1
sergey.anufriev <s.anufriev@rarible.com>
2021-11-13 15:04:33 +0700

    Add apan standart types

---
+++ apm/src/main/kotlin/com/rarible/core/apm/SpanType.kt
@@ -0,0 +1,9 @@
+package com.rarible.core.apm
+
+object SpanType {
+    const val DB: String = "db"
+    const val APP: String = "app"
+    const val EXT: String = "ext"
+    const val CACHE: String = "cache"
+    const val TEMPLATE: String = "template"
+}
