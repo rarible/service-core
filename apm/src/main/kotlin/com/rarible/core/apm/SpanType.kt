7743af181357655f00bf3f71e622e02b45c248ce
Artem Yakischik <a.yakischik@rarible.com>
2021-11-18 16:03:47 +0400

    Kafka/Event types added

--- apm/src/main/kotlin/com/rarible/core/apm/SpanType.kt
+++ apm/src/main/kotlin/com/rarible/core/apm/SpanType.kt
@@ -1,9 +1,14 @@
 package com.rarible.core.apm
 
 object SpanType {
+    // Default types of APM
     const val DB: String = "db"
     const val APP: String = "app"
     const val EXT: String = "ext"
     const val CACHE: String = "cache"
     const val TEMPLATE: String = "template"
+
+    // Custom types
+    const val KAFKA: String = "kafka" // To measure kafka interactions (publicEvent, for example)
+    const val EVENT: String = "event" // To measure event handling operations
 }
