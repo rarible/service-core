459bba2213bafdb93b460e6b88741ecabac58a3b
sergey.anufriev <s.anufriev@rarible.com>
2021-12-08 14:34:40 +0700

    RPN-1442 Remove Comparable from base Reduce interface

--- entity-reducer/src/main/kotlin/com/rarible/core/entity/reducer/service/Reducer.kt
+++ entity-reducer/src/main/kotlin/com/rarible/core/entity/reducer/service/Reducer.kt
@@ -1,6 +1,6 @@
 package com.rarible.core.entity.reducer.service
 
-interface Reducer<Event : Comparable<Event>, E> {
+interface Reducer<Event, E> {
     suspend fun reduce(entity: E, event: Event): E
 }
 
