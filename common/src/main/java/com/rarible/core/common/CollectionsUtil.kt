111d750c83f937587f8c361f04f89c55bc57be48
Andrey Atapin <a.atapin@gmail.com>
2022-01-23 17:06:22 +0500

    Shortcuts for parallel collections processing

---
+++ common/src/main/java/com/rarible/core/common/CollectionsUtil.kt
@@ -0,0 +1,23 @@
+@file:Suppress("SpellCheckingInspection")
+
+package com.rarible.core.common
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
+    map { async { f(it) } }.awaitAll()
+}
+
+suspend fun <A, B> Iterable<A>.pflatMap(f: suspend (A) -> Iterable<B>): List<B> = coroutineScope {
+    pmap(f).flatten()
+}
+
+suspend fun <K, A, B> Map<K, A>.pmap(f: suspend (Map.Entry<K, A>) -> B): List<B> = coroutineScope {
+    map { async { f(it) } }.awaitAll()
+}
+
+suspend fun <K, A, B> Map<K, A>.pflatMap(f: suspend (Map.Entry<K, A>) -> Iterable<B>): List<B> = coroutineScope {
+    pmap(f).flatten()
+}
---
+++ common/src/test/java/com/rarible/core/common/CollectionsUtilsTest.kt
@@ -0,0 +1,72 @@
+package com.rarible.core.common
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runBlockingTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+@ExperimentalCoroutinesApi
+class CollectionsUtilsTest {
+
+    @Test
+    fun `should map over iterable concurrently`() = runBlockingTest {
+        val iterable = (1..10000).toList()
+        val started = this.currentTime
+        val parResult = iterable.pmap {
+            delay(1000)
+            it.toString()
+        }
+        val ended = this.currentTime
+        val simpleResult = iterable.map { it.toString() }
+
+        Assertions.assertThat(ended - started).isEqualTo(1000)
+        Assertions.assertThat(parResult).containsExactlyElementsOf(simpleResult)
+    }
+
+    @Test
+    fun `should flatMap over iterable concurrently`() = runBlockingTest {
+        val iterable = (1..1000).toList()
+        val started = this.currentTime
+        val parResult = iterable.pflatMap {
+            delay(1000)
+            (it..it+10).toList()
+        }
+        val ended = this.currentTime
+        val simpleResult = iterable.flatMap { (it..it+10).toList() }
+
+        Assertions.assertThat(ended - started).isEqualTo(1000)
+        Assertions.assertThat(parResult).containsExactlyElementsOf(simpleResult)
+    }
+
+    @Test
+    fun `should map over Map concurrently`() = runBlockingTest {
+        val map = (1..10000).associateWith { "Value: $it" }
+        val started = this.currentTime
+        val parResult = map.pmap { (key, value) ->
+            delay(1000)
+            "$key=$value"
+        }
+        val ended = this.currentTime
+        val simpleResult = map.map { (key, value) -> "$key=$value" }
+
+        Assertions.assertThat(ended - started).isEqualTo(1000)
+        Assertions.assertThat(parResult).containsExactlyElementsOf(simpleResult)
+    }
+
+    @Test
+    fun `should flatMap over Map concurrently`() = runBlockingTest {
+        val map = (1..10000).associateWith { "Value: $it" }
+        val started = this.currentTime
+        val parResult = map.pflatMap { (key, value) ->
+            delay(1000)
+            listOf("$key=$value")
+        }
+        val ended = this.currentTime
+        val simpleResult = map.flatMap { (key, value) -> listOf("$key=$value") }
+
+        Assertions.assertThat(ended - started).isEqualTo(1000)
+        Assertions.assertThat(parResult).containsExactlyElementsOf(simpleResult)
+    }
+}
