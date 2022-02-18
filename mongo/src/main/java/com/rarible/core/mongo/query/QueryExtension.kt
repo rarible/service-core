94facb2a3c22d98c2fc9298ede810d7958eecc38
Roman Novikov <mystdeim@gmail.com>
2022-02-18 13:52:48 +0300

    fix typo

--- mongo/src/main/java/com/rarible/core/mongo/query/QueryExtension.kt
+++ mongo/src/main/java/com/rarible/core/mongo/query/QueryExtension.kt
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Component
 
 @Component
-class QueryExtention(
+class QueryExtension(
     @Value("\${rarible.core.mongo.maxTime.fast:3000}") private val fast: Long,
     @Value("\${rarible.core.mongo.maxTime.medium:50000}") private val medium: Long,
     @Value("\${rarible.core.mongo.maxTime.slow:600000}") private val slow: Long
@@ -18,12 +18,12 @@ class QueryExtention(
     }
 
     companion object {
-        private var FAST: Long = 0
-        private var MEDIUM: Long = 0
-        private var SLOW: Long = 0
+        var FAST: Long = 0
+        var MEDIUM: Long = 0
+        var SLOW: Long = 0
     }
-
-    fun Query.fast() = this.maxTimeMsec(FAST)
-    fun Query.medium() = this.maxTimeMsec(MEDIUM)
-    fun Query.slow() = this.maxTimeMsec(SLOW)
 }
+
+fun Query.fast() = this.maxTimeMsec(QueryExtension.FAST)
+fun Query.medium() = this.maxTimeMsec(QueryExtension.MEDIUM)
+fun Query.slow() = this.maxTimeMsec(QueryExtension.SLOW)
