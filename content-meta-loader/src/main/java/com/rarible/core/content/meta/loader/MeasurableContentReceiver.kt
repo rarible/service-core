4c80eafc95df0429805f9f1a9a0d1b5bbbdcee39
sanufriev <s.anufriev@rarible.com>
2022-04-01 18:34:24 +0700

    fix: Fix metrics

--- content-meta-loader/src/main/java/com/rarible/core/content/meta/loader/MeasurableContentReceiver.kt
+++ content-meta-loader/src/main/java/com/rarible/core/content/meta/loader/MeasurableContentReceiver.kt
@@ -8,13 +8,14 @@ class MeasurableContentReceiver(
     private val delegate: ContentReceiver,
     meterRegistry: MeterRegistry
 ) : ContentReceiver, Closeable {
-    private val receiveCallCounter = meterRegistry.counter("${PREFIX}_receiver_call")
+    private val receiveCallCounter = meterRegistry.counter("${PREFIX}_receiver_success")
     private val receiveErrorCounter = meterRegistry.counter("${PREFIX}_receiver_error")
 
     override suspend fun receiveBytes(url: URL, maxBytes: Int): ContentBytes {
-        receiveCallCounter.increment()
         return try {
-            delegate.receiveBytes(url, maxBytes)
+            delegate.receiveBytes(url, maxBytes).also {
+                receiveCallCounter.increment()
+            }
         } catch (ex: Throwable) {
             receiveErrorCounter.increment()
             throw ex
