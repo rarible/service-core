b0c8600503c2f3a053e792128e5218eb98a4f79a
Mikhail Nasimov <m.nasimov@rarible.com>
2022-05-15 00:41:07 +0300

    Add support for aarch64 kafka on m1

--- test-common/src/main/java/com/rarible/core/test/containers/KafkaTestContainer.kt
+++ test-common/src/main/java/com/rarible/core/test/containers/KafkaTestContainer.kt
@@ -10,7 +10,9 @@ class KafkaTestContainer {
     }
 
     companion object {
-        val KAFKA__IMAGE: DockerImageName =
+        val KAFKA__IMAGE: DockerImageName = if (System.getProperty("os.arch") == "aarch64")
+            DockerImageName.parse("ghcr.io/arm64-compat/confluentinc/cp-kafka:7.1.1").asCompatibleSubstituteFor("confluentinc/cp-kafka")
+        else
             DockerImageName.parse("confluentinc/cp-kafka:6.1.1")
 
         @JvmStatic
