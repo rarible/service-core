9451207af5239c2da65951d17efd78d5d1e5f90f
Alexey Kuryshev <otr@inbox.ru>
2022-08-02 10:10:58 +0300

    MP-3225 cr notes

--- telemetry/src/main/java/com/rarible/core/telemetry/logging/LogLayout.kt
+++ telemetry/src/main/java/com/rarible/core/telemetry/logging/LogLayout.kt
@@ -1,43 +1,37 @@
 package com.rarible.core.telemetry.logging
 
 import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.classic.spi.ThrowableProxyUtil
 import com.batch.escalog.LogFmtLayout
 
 class LogLayout : LogFmtLayout() {
     override fun doLayout(iLoggingEvent: ILoggingEvent?): String {
         val log = super.doLayout(iLoggingEvent)
         val extraLength = log.length - MAX_LENGTH
-        return if (extraLength < 0) log else cut(log, extraLength)
+        return if (extraLength < 0) log else cut(log, iLoggingEvent, extraLength)
     }
 
-    private fun cut(origLog: String, length: Int): String {
-        val cutLog = cutLogByTab(origLog, ERROR_TAG, length)
-        val extraLength = cutLog.length - MAX_LENGTH
-        return if (extraLength < 0) cutLog else cutLogByTab(cutLog, MSG_TAG, extraLength)
+    private fun cut(originalLog: String, iLoggingEvent: ILoggingEvent?, length: Int): String {
+        if (iLoggingEvent == null) return originalLog
+
+        val cutErrorLog = cutLogValue(
+            originalLog,
+            escapeValue(ThrowableProxyUtil.asString(iLoggingEvent.throwableProxy)).toString(),
+            length
+        )
+        val extraLength = cutErrorLog.length - MAX_LENGTH
+
+        return if (extraLength < 0)
+            cutErrorLog
+        else
+            cutLogValue(cutErrorLog, escapeValue(iLoggingEvent.message).toString(), extraLength)
     }
 
-    private fun cutLogByTab(log: String, tag: String, length: Int): String {
-        val value = getValue(log, tag)
-        return if (value != null) log.replace(value, value.dropLast(length)) else log
-    }
-
-    private fun getValue(log: String, tag: String): String? {
-        val index = log.indexOf(tag)
-        if (index > 0) {
-            val messageStartIndex = index + tag.length
-            return if (log.getOrNull(messageStartIndex) == '"') {
-                log.substring(messageStartIndex + 1, log.indexOf('"', messageStartIndex + 1))
-            } else {
-                log.substring(messageStartIndex, log.indexOf(' ', messageStartIndex))
-            }
-        } else {
-            return null
-        }
+    private fun cutLogValue(log: String, value: String?, length: Int): String {
+        return if (!value.isNullOrEmpty()) log.replace(value, value.dropLast(length)) else log
     }
 
     companion object {
         private const val MAX_LENGTH = 16000
-        private const val MSG_TAG = "msg="
-        private const val ERROR_TAG = "error="
     }
 }
