4bb230d9bc5d685009ef70eac1c50c4cb098e025
Artem Yakischik <a.yakischik@rarible.com>
2022-09-19 18:29:53 +0400

    feat: Metrics tag contributor for client

---
+++ telemetry/src/main/java/com/rarible/core/telemetry/actuator/WebRequestClientTagContributor.kt
@@ -0,0 +1,54 @@
+package com.rarible.core.telemetry.actuator
+
+import io.micrometer.core.instrument.Tag
+import org.springframework.boot.actuate.metrics.web.reactive.server.WebFluxTagsContributor
+import org.springframework.web.server.ServerWebExchange
+
+/**
+ * Client name Tag contributor for the API metrics. Required to distinguish API calls from different clients.
+ * Basically, we want to see 'rarible'/'other' distribution. In the future, we may extend set of clients to track.
+ * We need to be VERY careful here in terms of clientName's possible values set. Prometheus might not be able
+ * to handle them.
+ */
+class WebRequestClientTagContributor(
+    private val clientNameHeader: String = "x-rarible-client",
+    // By default, we're tracking only 'rarible' client, all others will be tagged as 'other'
+    private val clientNameFilter: (String) -> Boolean = { it == RARIBLE },
+    // Default simple implementation to detect requests from marketplace by origin
+    private val clientNameExtractor: (String) -> String? = { origin -> getFromOrigin(origin) }
+) : WebFluxTagsContributor {
+
+    override fun httpRequestTags(exchange: ServerWebExchange?, ex: Throwable?): MutableIterable<Tag>? {
+        exchange ?: return null
+
+        val clientName = exchange.request.headers.getFirst(clientNameHeader)
+        clientName?.let { return toClientTag(it) }
+
+        val originClientName = exchange.request.headers.origin?.let { clientNameExtractor(it) } ?: OTHER
+        return toClientTag(originClientName)
+    }
+
+    private fun toClientTag(clientName: String): MutableIterable<Tag> {
+        val result = if (clientNameFilter(clientName)) {
+            clientName
+        } else {
+            OTHER
+        }
+        return mutableListOf(Tag.of("client", result))
+    }
+
+    companion object {
+
+        private const val OTHER = "other"
+        private const val RARIBLE = "rarible"
+
+        private fun getFromOrigin(origin: String): String? {
+            return if (origin.endsWith("rarible.com")) {
+                RARIBLE
+            } else {
+                null
+            }
+        }
+    }
+
+}
---
+++ telemetry/src/main/test/java/com/rarible/core/telemetry/actuator/WebRequestClientTagContributorTest.kt
@@ -0,0 +1,61 @@
+package com.rarible.core.telemetry.actuator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest
+import org.springframework.mock.web.server.MockServerWebExchange
+import org.springframework.web.server.ServerWebExchange
+
+class WebRequestClientTagContributorTest {
+
+    private val contributor = WebRequestClientTagContributor()
+
+    @Test
+    fun `client name taken from header`() {
+        val exchange = createExchange(clientName = "rarible")
+        val tag = contributor.httpRequestTags(exchange, null)!!.first()
+        assertThat(tag.value).isEqualTo("rarible")
+    }
+
+    @Test
+    fun `client name taken from origin`() {
+        val exchange = createExchange(origin = "https://dev.rarible.com")
+        val tag = contributor.httpRequestTags(exchange, null)!!.first()
+        assertThat(tag.value).isEqualTo("rarible")
+    }
+
+    @Test
+    fun `client name filtered from header`() {
+        val exchange = createExchange(clientName = "not-rarible")
+        val tag = contributor.httpRequestTags(exchange, null)!!.first()
+        assertThat(tag.value).isEqualTo("other")
+    }
+
+    @Test
+    fun `client name filtered from origin`() {
+        val exchange = createExchange(origin = "https://something.com")
+        val tag = contributor.httpRequestTags(exchange, null)!!.first()
+        assertThat(tag.value).isEqualTo("other")
+    }
+
+    @Test
+    fun `client name filtered from header, origin not checked`() {
+        // Origin can be parsed to clientName,
+        // but since there is non-applicable clientName in header, it should be omitted
+        val exchange = createExchange(origin = "https://rarible.com", clientName = "not-rarible")
+        val tag = contributor.httpRequestTags(exchange, null)!!.first()
+        assertThat(tag.value).isEqualTo("other")
+    }
+
+    private fun createExchange(
+        origin: String? = null,
+        clientName: String? = null
+    ): ServerWebExchange {
+        val request = MockServerHttpRequest.get("/")
+        origin?.let { request.header(HttpHeaders.ORIGIN, origin) }
+        clientName?.let { request.header("x-rarible-client", clientName) }
+        return MockServerWebExchange.builder(request.build()).build()
+    }
+
+}
