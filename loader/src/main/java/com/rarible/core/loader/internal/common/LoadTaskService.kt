17ded32d544f0923f0aab303a215410ae252c1b3
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2022-10-21 17:24:00 +0400

    Listener required for the meta migration (#63)

--- loader/src/main/java/com/rarible/core/loader/internal/common/LoadTaskService.kt
+++ loader/src/main/java/com/rarible/core/loader/internal/common/LoadTaskService.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.flow.Flow
 import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.stereotype.Component
 import java.time.Instant
-import kotlin.jvm.Throws
 
 interface LoadTaskService {
     fun getAll(): Flow<LoadTask>
@@ -18,8 +17,10 @@ interface LoadTaskService {
 
 @Component
 class LoadTaskServiceImpl(
-    private val loadTaskRepository: LoadTaskRepository
+    private val loadTaskRepository: LoadTaskRepository,
+    private val updateListeners: List<LoadTaskUpdateListener>
 ) : LoadTaskService {
+
     override fun getAll(): Flow<LoadTask> =
         loadTaskRepository.getAll()
 
@@ -29,8 +30,11 @@ class LoadTaskServiceImpl(
     override fun getTasksToRetry(now: Instant): Flow<LoadTask> =
         loadTaskRepository.getTasksToRetry(now)
 
-    override suspend fun save(task: LoadTask): LoadTask =
-        loadTaskRepository.save(task)
+    override suspend fun save(task: LoadTask): LoadTask {
+        val result = loadTaskRepository.save(task)
+        updateListeners.forEach { it.onTaskSaved(result) }
+        return result
+    }
 
     override suspend fun remove(task: LoadTask) =
         loadTaskRepository.remove(task)
