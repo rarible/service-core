58060be2d8fa6f67c00a74c635ae5fa48d9be617
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2023-06-14 16:33:02 +0300

    feat(PT-3039): improve worker metrics (#80)

--- daemon/src/main/java/com/rarible/core/daemon/DaemonWorkerMetrics.kt
+++ daemon/src/main/java/com/rarible/core/daemon/DaemonWorkerMetrics.kt
@@ -2,27 +2,36 @@ package com.rarible.core.daemon
 
 import com.rarible.core.telemetry.metrics.CountingMetric
 
+private const val METRIC_NAME = "worker_event"
+private const val WORKER_TAG_NAME = "worker"
+private const val EVENT_TAG_NAME = "event"
+
 class DaemonIncome(workerName: String) : CountingMetric(
-    workerName,
-    tag("event", "income")
+    METRIC_NAME,
+    tag(WORKER_TAG_NAME, workerName),
+    tag(EVENT_TAG_NAME, "income")
 )
 
 class DaemonLiveness(workerName: String) : CountingMetric(
-    workerName,
-    tag("event", "alive")
+    METRIC_NAME,
+    tag(WORKER_TAG_NAME, workerName),
+    tag(EVENT_TAG_NAME, "alive")
 )
 
 class DaemonError(workerName: String) : CountingMetric(
-    workerName,
-    tag("event", "execution_error")
+    METRIC_NAME,
+    tag(WORKER_TAG_NAME, workerName),
+    tag(EVENT_TAG_NAME, "execution_error")
 )
 
 class DaemonProcessingError(workerName: String) : CountingMetric(
-    workerName,
-    tag("event", "processing_error")
+    METRIC_NAME,
+    tag(WORKER_TAG_NAME, workerName),
+    tag(EVENT_TAG_NAME, "processing_error")
 )
 
 class DaemonClosedChannelEvent(workerName: String) : CountingMetric(
-    workerName,
-    tag("event", "closed_channel")
+    METRIC_NAME,
+    tag(WORKER_TAG_NAME, workerName),
+    tag(EVENT_TAG_NAME, "closed_channel")
 )
