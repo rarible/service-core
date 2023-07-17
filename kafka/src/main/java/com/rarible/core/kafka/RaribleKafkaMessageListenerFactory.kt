ccfbab1a4b9e85cd8b18a4c62cecce98b25b18b4
Artem Yakischik <a.yakischik@rarible.com>
2023-07-17 14:47:17 +0400

    feat: Kafka async sub-batches processing

--- kafka/src/main/java/com/rarible/core/kafka/RaribleKafkaMessageListenerFactory.kt
+++ kafka/src/main/java/com/rarible/core/kafka/RaribleKafkaMessageListenerFactory.kt
@@ -15,13 +15,20 @@ object RaribleKafkaMessageListenerFactory {
 
     fun <T> create(handler: RaribleKafkaEventHandler<T>, async: Boolean): BatchMessageListener<String, T> {
         return when (async) {
+            // All events will be handled in parallel (except that having same key)
             true -> Concurrent(handler)
+            // All events will be handled one-by-one
             false -> Sequential(handler)
         }
     }
 
-    fun <T> create(handler: RaribleKafkaBatchEventHandler<T>): BatchMessageListener<String, T> {
-        return Batch(handler)
+    fun <T> create(handler: RaribleKafkaBatchEventHandler<T>, async: Boolean): BatchMessageListener<String, T> {
+        return when (async) {
+            // All events grouped by key will be handled in parallel, all events in the batch have same key
+            true -> BatchConcurrent(handler)
+            // Batch will be handled entirely by handler, concurrency implementation lies on handler's implementation
+            false -> BatchSequential(handler)
+        }
     }
 
     private class Concurrent<T>(
@@ -60,7 +67,7 @@ object RaribleKafkaMessageListenerFactory {
         }
     }
 
-    private class Batch<T>(
+    private class BatchSequential<T>(
         private val handler: RaribleKafkaBatchEventHandler<T>
     ) : BatchMessageListener<String, T> {
 
@@ -69,6 +76,24 @@ object RaribleKafkaMessageListenerFactory {
                 withTraceId {
                     handler.handle(records.mapNotNull { it.value() })
                 }
+            }
+        }
+    }
+
+    private class BatchConcurrent<T>(
+        private val handler: RaribleKafkaBatchEventHandler<T>
+    ) : BatchMessageListener<String, T> {
+
+        override fun onMessage(records: List<ConsumerRecord<String, T>>) = runBlocking<Unit>(NonCancellable) {
+            withBatchId {
+                val recordsByKey = records.filter { it.value() != null }.groupBy { it.key() }
+                recordsByKey.values.map { group ->
+                    asyncWithTraceId(context = NonCancellable) {
+                        withTraceId {
+                            handler.handle(group.map { it.value() })
+                        }
+                    }
+                }.awaitAll()
             }
         }
     }
