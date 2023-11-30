324f3cb13e222560a4c7b7b4815ff2b4708fb77b
Roman Novikov <mystdeim@gmail.com>
2023-11-30 09:31:32 +0000

    feat: add kafka cleanup (#107)

---
+++ test-common/src/main/java/com/rarible/core/test/ext/KafkaCleanup.kt
@@ -0,0 +1,40 @@
+package com.rarible.core.test.ext
+
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.ConsumerGroupListing
+import org.apache.kafka.clients.admin.ListTopicsResult
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.TopicCollection
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+import java.lang.annotation.Inherited
+import java.util.stream.Collectors
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Inherited
+@ExtendWith(KafkaCleanupExtension::class)
+annotation class KafkaCleanup
+
+class KafkaCleanupExtension : BeforeAllCallback {
+
+    override fun beforeAll(context: ExtensionContext?) {
+        AdminClient.create(mapOf(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to KafkaTestExtension.kafkaContainer.kafkaBoostrapServers())).use { adminClient ->
+
+            // Delete all topics
+            val result: ListTopicsResult = adminClient.listTopics()
+            val topicNames = result.names().get()
+            topicNames.remove("_schemas")
+            adminClient.deleteTopics(TopicCollection.ofTopicNames(topicNames))
+
+            // Delete all consumer groups
+            val consumerGroups: List<String> = adminClient
+                .listConsumerGroups().all().get()
+                .stream()
+                .map { obj: ConsumerGroupListing -> obj.groupId() }
+                .collect(Collectors.toList())
+            adminClient.deleteConsumerGroups(consumerGroups)
+        }
+    }
+}
