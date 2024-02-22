ad8fba61a4c4d05d671fae98004cc4c90f6211d1
Dmitry Saraykin <d.saraykin@rarible.com>
2024-02-22 17:45:18 +0300

    feat(PT-4871): configure mongo timeouts

---
+++ mongo/src/main/java/com/rarible/core/mongo/configuration/MongoProperties.kt
@@ -0,0 +1,12 @@
+package com.rarible.core.mongo.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.time.Duration
+
+@ConstructorBinding
+@ConfigurationProperties("rarible.core.mongodb")
+data class MongoProperties(
+    val maxConnectionLifeTime: Duration? = null,
+    val maxConnectionIdleTime: Duration? = null,
+)
