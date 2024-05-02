2684fde5a7d7a18cf87373d3acda168520f81881
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2024-05-02 12:46:32 +0300

    feat(PT-5308): mongo query max time (#119)
    * feat(PT-5308): mongo query max time
    
    * feat(PT-5308): mongo query max time
    
    * feat(PT-5308): mongo query max time

---
+++ mongo/src/main/java/com/rarible/core/mongo/template/RaribleReactiveMongoTemplate.kt
@@ -0,0 +1,39 @@
+package com.rarible.core.mongo.template
+
+import com.mongodb.reactivestreams.client.FindPublisher
+import com.rarible.core.mongo.configuration.MongoProperties
+import org.bson.Document
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.core.FindPublisherPreparer
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.convert.MongoConverter
+import reactor.core.publisher.Flux
+import java.util.concurrent.TimeUnit
+
+class RaribleReactiveMongoTemplate(
+    reactiveMongoDatabaseFactory: ReactiveMongoDatabaseFactory,
+    converter: MongoConverter,
+    private val properties: MongoProperties,
+) : ReactiveMongoTemplate(reactiveMongoDatabaseFactory, converter) {
+
+    override fun <T : Any?> doFind(
+        collectionName: String?,
+        query: Document?,
+        fields: Document?,
+        entityClass: Class<T>?,
+        preparer: FindPublisherPreparer?
+    ): Flux<T> {
+        return super.doFind(collectionName, query, fields, entityClass, MaxTimeAwarePreparer(preparer))
+    }
+
+    inner class MaxTimeAwarePreparer(private val delegate: FindPublisherPreparer?) : FindPublisherPreparer {
+        override fun prepare(findPublisher: FindPublisher<Document>): FindPublisher<Document> {
+            val publisher = if (properties.maxTime != null) {
+                findPublisher.maxTime(properties.maxTime.toMillis(), TimeUnit.MILLISECONDS)
+            } else {
+                findPublisher
+            }
+            return delegate?.prepare(publisher) ?: publisher
+        }
+    }
+}
