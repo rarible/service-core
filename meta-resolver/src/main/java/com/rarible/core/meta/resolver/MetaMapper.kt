19cb0e8894af434e12ff124a6672c1740061c57e
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2024-05-20 11:12:47 +0400

    Minor refactoring (#122)

--- meta-resolver/src/main/java/com/rarible/core/meta/resolver/MetaMapper.kt
+++ meta-resolver/src/main/java/com/rarible/core/meta/resolver/MetaMapper.kt
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.node.ObjectNode
 /**
  * Metadata mapper from raw JSON to some DTO
  */
-interface MetaMapper<K, M : Meta> {
+interface MetaMapper<K, M> {
 
     fun map(entityId: K, json: ObjectNode): M
+
+    fun isEmpty(meta: M): Boolean
 }
--- meta-resolver/src/test/java/com/rarible/core/meta/resolver/test/TestMeta.kt
+++ meta-resolver/src/test/java/com/rarible/core/meta/resolver/test/TestMeta.kt
@@ -1,9 +1,3 @@
 package com.rarible.core.meta.resolver.test
 
-import com.rarible.core.meta.resolver.Meta
-
-data class TestMeta(val name: String?) : Meta {
-    override fun isEmpty(): Boolean {
-        return name == null
-    }
-}
+data class TestMeta(val name: String?)
