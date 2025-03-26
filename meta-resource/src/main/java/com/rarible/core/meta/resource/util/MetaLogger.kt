976a130a34614ecc07600c01d81794259ae8fb91
Igor Baltiyskiy <i.baltiyskiy@rarible.com>
2025-03-26 16:49:30 +0100

    PT-7163 Add an option to log exception in logMetaLoading()

--- meta-resource/src/main/java/com/rarible/core/meta/resource/util/MetaLogger.kt
+++ meta-resource/src/main/java/com/rarible/core/meta/resource/util/MetaLogger.kt
@@ -7,16 +7,16 @@ object MetaLogger {
 
     private val itemMetaLogger: Logger = LoggerFactory.getLogger("item-meta-loading")
 
-    fun <T> logMetaLoading(id: T, message: String, warn: Boolean = false) {
-        itemMetaLogger.logMetaLoading(id.toString(), message, warn)
+    fun <T> logMetaLoading(id: T, message: String, warn: Boolean = false, exception: Throwable? = null) {
+        itemMetaLogger.logMetaLoading(id.toString(), message, warn, exception)
     }
 
-    fun Logger.logMetaLoading(id: String, message: String, warn: Boolean = false) {
+    fun Logger.logMetaLoading(id: String, message: String, warn: Boolean = false, exception: Throwable? = null) {
         val logMessage = "Meta of $id: $message"
         if (warn) {
-            this.warn(logMessage)
+            this.warn(logMessage, exception)
         } else {
-            this.info(logMessage)
+            this.info(logMessage, exception)
         }
     }
 }
